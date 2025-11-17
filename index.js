43bc776ba39cd353843753d49bb24a313112a708
Vincent Ganiza <vincentganiza9@gmail.com>
2025-11-17 18:46:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star SHABAN-SOBX-MD
+Don't Forgot To Give Star GAGA-MD 
 
  Server running on http://localhost:` + PORT)
 })
